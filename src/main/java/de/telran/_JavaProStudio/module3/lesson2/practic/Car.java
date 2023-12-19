32682cf4b0ae9d84417a1c9481dfd3433b136455
shemchuk <shemchukaga@gmail.com>
2023-12-19 20:17:36 +0100

    _19_12 lebory Function

---
+++ src/main/java/de/telran/_JavaProStudio/module3/lesson2/practic/Car.java
@@ -0,0 +1,15 @@
+package de.telran._JavaProStudio.module3.lesson2.practic;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+@AllArgsConstructor
+@Getter
+
+public class Car {
+        private String number;
+        private String name;
+        private double price;
+    }
+
