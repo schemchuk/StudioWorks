1ae29e5690d45e066523e9e43e9caf7d92f10efc
shemchuk <shemchukaga@gmail.com>
2024-05-07 12:53:34 +0200

    game work

---
+++ src/main/java/de/telran/algorytm/lesson_24_05_07/SimpleString.java
@@ -0,0 +1,8 @@
+package de.telran.algorytm.lesson_24_05_07;
+
+public class SimpleString {
+    public static void main(String[] args) {
+        int[] arr1 = {12,4,8,17,9,40,1};
+        int[] arr2 = {0,0,0,0,0,0,0};
+    }
+}
